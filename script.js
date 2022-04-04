dd3ea62788936a90a1d1b5b54ee79227e66145e2
Syd <98357136+SydAgui@users.noreply.github.com>
2022-04-04 21:19:57 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+$( function() {
+    $( "#draggable" ).draggable();
+    $( "#droppable" ).droppable({
+      drop: function( event, ui ) {
+        $( this )
+          .addClass( "ui-state-highlight" )
+          .find( "img" )
+            .html( "cart.png" );
+      }
+    });
+  } );
